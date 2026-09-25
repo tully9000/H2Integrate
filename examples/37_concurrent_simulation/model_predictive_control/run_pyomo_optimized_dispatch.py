d2a42216d0b35f133ba17cca65497c66118f89ed
Zachary Tully <ztully@nlr.gov>
2026-09-25 10:58:20 -0600

    Setting up pyomo in SLC example

--- examples/37_concurrent_simulation/model_predictive_control/run_pyomo_optimized_dispatch.py
+++ examples/37_concurrent_simulation/model_predictive_control/run_pyomo_optimized_dispatch.py
@@ -1,23 +1,88 @@
+from pathlib import Path
+
 import numpy as np
 from matplotlib import pyplot as plt
 
 from h2integrate import H2IntegrateModel
 
 
+pyomo_dispatch_config = (
+    Path(__file__).parent / "pyomo_optimized_dispatch" / "pyomo_optimized_dispatch.yaml"
+)
+slc_dispatch_config = (
+    Path(__file__).parent / "SLC_optimized_dispatch" / "pyomo_optimized_dispatch.yaml"
+)
+
 # Create an H2Integrate model
-model = H2IntegrateModel("pyomo_optimized_dispatch.yaml")
+model = H2IntegrateModel(pyomo_dispatch_config)
 
 demand_profile = np.ones(8760) * 100.0
-
-
 # TODO: Update with demand module once it is developed
 model.setup()
 model.prob.set_val("battery.electricity_set_point", demand_profile, units="MW")
-
 # Run the model
 model.run()
-
 model.post_process()
+
+# Create an H2Integrate model
+model = H2IntegrateModel(slc_dispatch_config)
+
+demand_profile = np.ones(8760) * 100.0
+# TODO: Update with demand module once it is developed
+model.setup()
+model.prob.set_val("battery.electricity_set_point", demand_profile, units="MW")
+# Run the model
+model.run()
+model.post_process()
+
+inputs = dict(model.model.list_inputs(out_stream=None))
+outputs = dict(model.model.list_outputs(out_stream=None))
+
+
+battery_electricity_out = outputs["plant.battery.PySAMBatteryPerformanceModel.electricity_out"][
+    "val"
+]
+demand_electricity_out = outputs[
+    "plant.electrical_load_demand.GenericDemandComponent.electricity_out"
+]["val"]
+demand_electricity_curtailed = outputs[
+    "plant.electrical_load_demand.GenericDemandComponent.unused_electricity_out"
+]["val"]
+demand_electricity_unmet = outputs[
+    "plant.electrical_load_demand.GenericDemandComponent.unmet_electricity_demand_out"
+]["val"]
+wind_electricity_out = outputs["plant.wind.PYSAMWindPlantPerformanceModel.electricity_out"]["val"]
+
+fig, ax = plt.subplots(3, 1, sharex="all", layout="constrained")
+
+time = np.arange(0, len(demand_electricity_out), 1)
+
+
+def fb(ax, top, bottom=None, kw={}):
+    time = np.arange(0, len(top), 1)
+
+    if bottom is None:
+        bottom = np.zeros(len(top))
+
+    ax.fill_between(time, bottom, bottom + top, step="post", **kw)
+
+
+kw = {"alpha": 0.5}
+
+fb(ax[0], wind_electricity_out, kw=kw)
+fb(ax[1], battery_electricity_out, kw=kw)
+
+fb(ax[2], wind_electricity_out, kw=kw)
+fb(ax[2], battery_electricity_out, wind_electricity_out, kw=kw)
+
+
+ax[2].step(
+    time,
+    inputs["plant.electrical_load_demand.GenericDemandComponent.electricity_demand"]["val"],
+    where="post",
+    color="black",
+)
+
 
 # Plot the results
 fig, ax = plt.subplots(2, 1, sharex=True, figsize=(8, 6))
@@ -41,12 +106,7 @@ ax[1].plot(
     linestyle="-",
     label="Electricity In (MW)",
 )
-ax[1].plot(
-    range(start_hour, end_hour),
-    model.prob.get_val("battery.unused_electricity_out", units="MW")[start_hour:end_hour],
-    linestyle=":",
-    label="Unused Electricity (MW)",
-)
+
 ax[1].plot(
     range(start_hour, end_hour),
     model.prob.get_val("battery.unmet_electricity_demand_out", units="MW")[start_hour:end_hour],
@@ -77,4 +137,4 @@ ax[1].set_xlabel("Timestep (hr)")
 
 plt.legend(ncol=2, frameon=False)
 plt.tight_layout()
-plt.savefig("optimized_dispatch_plot.png", dpi=300)
+# plt.savefig("optimized_dispatch_plot.png", dpi=300)
